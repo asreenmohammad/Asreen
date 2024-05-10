113eb0c5d1d367cc62d25e99daec7a7a1ea3656c
asreen <asreen.mohammad@jukshio.com>
2024-05-10 17:42:03 +0530

    add new code

--- rough.py
+++ rough.py
@@ -1,8 +1,15 @@
-num = list(range(10))
-previousNum = 0
-for i in num:
-    sum = previousNum + i
-    print('Current Number '+ str(i) + 'Previous Number ' + str(previousNum) + 'is ' + str(sum))
-    previousNum=i
+import argparse
+parser = argparse.ArgumentParser(description='process some integers')
+
+parser.add_argument('current_number', type=int)
+parser.add_argument('previous_number', type=int)
+args = parser.parse_args()
+current_num = args.current_number
+previous_num = args.previous_number
+
+for i in range(10):
+    sum = previous_num + i
+    print(f'Current number {i} Previous Number {previous_num} is {sum}')
+    previous_num = i
 
 
