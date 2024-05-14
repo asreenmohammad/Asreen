81565dbde3347f678fa28610a3b8ef7783f0cd6d
asreen <asreen.mohammad@jukshio.com>
2024-05-14 12:23:23 +0530

    add new code

--- pic.py
+++ pic.py
@@ -11,12 +11,21 @@ args = vars(parser.parse_args())
 image = cv2.imread(args['image'])
 
  
+<<<<<<< HEAD
 #img = cv.imread('doo.jpeg')
 #cv.imwrite("/home/asreen-mohammad/Downloads/doraemon.jpeg",img)
+=======
+#img = cv.imread('lotus.jpeg')
+#cv.imwrite("/home/bhumika-avadutha/Desktop/programs/b.jpeg",img)
+>>>>>>> 3af4647... add new code
 assert image is not None, "file could not be read, check with os.path.exists()"
 color = ('b','g','r')
 for i,col in enumerate(color):
  histr = cv2.calcHist([image],[i],None,[256],[0,256])
  plt.plot(histr,color = col)
  plt.xlim([0,256])
+<<<<<<< HEAD
 plt.show()
+=======
+plt.show()
+>>>>>>> 3af4647... add new code
